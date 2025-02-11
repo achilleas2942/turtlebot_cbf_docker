349fc293dab7084491f855656d67866f91708f90
Achilleas Seisa <achilleas2942@gmail.com>
2025-02-11 09:32:57 +0100

    (1) Add parameters for the number of robots, ROS_MASTER_URI, and ROS_IP (2) Fix git clone url (3) Fix exports for ROS_MASTER_URI and ROS_IP (4) Add roslaunch command

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
-set -e
 
 # Source the ROS setup
 source /opt/ros/noetic/setup.bash
+
+# Set the parameter
+Turtle_No=${1:-1}
+ROS_MASTER_IP=${2:-$(hostname -I | awk '{print $1}')}
+ROS_IP=${3:-$ROS_MASTER_IP}
 
 # Pull the latest updates from the repositories
 cd /root/catkin_ws/src
 if [ -d "tb_cbf" ]; then
     cd tb_cbf && git pull && cd ..
 else
-    git clone git@github.com:viswans2132/tb_cbf.git
-fi
-
-if [ -d "libviconstream" ]; then
-    cd libviconstream && git pull && cd ..
-else
-    git clone git@github.com:LTU-RAI/libviconstream.git
-fi
-
-if [ -d "ros_viconstream" ]; then
-    cd ros_viconstream && git pull && cd ..
-else
-    git clone git@github.com:LTU-RAI/ros_viconstream.git
+    git clone https://github.com/viswans2132/tb_cbf.git
 fi
 
 # Build the workspace
@@ -31,11 +23,9 @@ catkin_make
 # Source the workspace
 source /root/catkin_ws/devel/setup.bash
 
-# Export ROS_MASTER_URI
-export ROS_MASTER_URI=http://130.240.96.104:11311
-
-# Automatically detect and export the host IP for ROS_IP
-export ROS_IP=$(ip route | grep default | awk '{print $3}')
+# Export ROS_MASTER_URI and ROS_IP
+export ROS_MASTER_URI=http://"$ROS_MASTER_IP":11311
+export ROS_IP="$ROS_IP"
 
 # Execute the provided command
-exec "$@"
+roslaunch tb_cbf tro_docker_tb_cont.launch namespace:="demo_turtle"$Turtle_No""
